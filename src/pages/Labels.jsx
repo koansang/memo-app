a2d554bd32c4930653898cc590dc738ab70fdb72
koansang <koansang@gmail.com>
2019-10-03 04:33:00 +0900

    Feat: 기본페이지 적용

---
+++ src/pages/Labels.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Labels() {
+  return (
+    <div>
+      <h1>Label</h1>
+    </div>
+  );
+}
+
+export default Labels;
---
+++ src/pages/Memos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Memos() {
+  return (
+    <div>
+      <h1>Label</h1>
+    </div>
+  );
+}
+
+export default Memos;
